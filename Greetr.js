8d210f6a8cd12192aed50a7e025c8501007b8c06
TUSHYA TRIVEDI <ttushya@gamil.com>
2026-03-20 13:52:59 +0530

    setting up initial structure to return Greetr object

--- Greetr.js
+++ Greetr.js
@@ -1,1 +1,9 @@
-(function (window, $) {})(window, $);
+//IIFE gives own execution context so that our variables dont collide with global ones
+(function (global, $) {
+  //Following pattern similar to jQuery where we don't have to use new keyword to create a new jQuery object
+  var Greetr = function (firstname, lastname, language) {
+    return new Greetr.init(firstname, lastname, language);
+  };
+
+  Greetr.init = function (firstname, lastname, language) {};
+})(window, $);
