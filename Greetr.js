93156654b3de604a2d622fe2921382fb30db03b0
TUSHYA TRIVEDI <ttushya@gamil.com>
2026-03-20 14:15:23 +0530

    added functionality to setup prototype of the object returned Also setup an alias for the object by adding it to the global object

--- Greetr.js
+++ Greetr.js
@@ -5,5 +5,21 @@
     return new Greetr.init(firstname, lastname, language);
   };
 
-  Greetr.init = function (firstname, lastname, language) {};
+  //contains methods that can be used on the object Greetr
+  //automatically assigned to the object created using the function constructor
+  Greetr.prototype = {};
+
+  //'this' points to new empty object created using function constructor
+  Greetr.init = function (firstname, lastname, language) {
+    var self = this;
+    self.firstname = firstname || "";
+    self.lastname = lastname || "";
+    self.language = language || "en";
+  };
+  //since function constructor return new Greetr.init() object, prototype needs to be setup
+  Greetr.init.prototype = Greetr.prototype;
+
+  //I want to use G$() to initialize any object, so I point it to Greetr function object
+  //and add it to the global object
+  global.G$ = global.Greetr = Greetr;
 })(window, $);
---
+++ app.js
@@ -0,0 +1,1 @@
+var greetr = G$("default", "default", "en");
